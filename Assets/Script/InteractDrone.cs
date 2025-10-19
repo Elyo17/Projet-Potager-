46b99163967015e45432a2f34b613e40a46750ff
Elyo <maamarielyo@gmail.com>
2025-10-19 21:26:09 +0200

    ajout du script pour les intéractions

---
+++ Assets/Script/InteractDrone.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InteractDrone : MonoBehaviour
+{
+
+    public InputActionReference ActionReference;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    private void OnEnable()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
